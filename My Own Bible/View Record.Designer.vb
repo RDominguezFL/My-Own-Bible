d1de704c0abeb9e10f647de9231001d6809a909c
RDominguezFL <rdom6031@interponce.edu>
2018-05-26 02:25:20 -0400

    Update_01

--- My Own Bible/View Record.Designer.vb
+++ My Own Bible/View Record.Designer.vb
@@ -236,7 +236,7 @@ Partial Class Form3
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.My_Own_Bible.My.Resources.Resources.back2
-        Me.ClientSize = New System.Drawing.Size(516, 399)
+        Me.ClientSize = New System.Drawing.Size(541, 411)
         Me.ControlBox = False
         Me.Controls.Add(Me.Lable11)
         Me.Controls.Add(Me.lblphone)
